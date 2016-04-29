fd20a9d6b6354137f7b34d15cde83892eaf4b3d4
yeqinfu <990761790@qq.com>
2016-04-29 16:32:55 +0800

    !http

--- app/src/main/java/com/ppandroid/whitefm/http/ET_HttpError.java
+++ app/src/main/java/com/ppandroid/whitefm/http/ET_HttpError.java
@@ -8,38 +8,38 @@ import com.ppandroid.whitefm.http.bean.BN_BaseBody;
  */
 public class ET_HttpError {
 
-	public int						taskId;
+    public int						taskId;
 
-	/**
-	 * 9001 ????<br>
-	 * 9002 ?????<br>
-	 * --???????????
-	 */
-	public int						errorCode;
+    /**
+     * 9001 ????<br>
+     * 9002 ?????<br>
+     * --???????????
+     */
+    public int						errorCode;
 
-	public String errorDescription;
+    public String					errorDescription;
 
-	/**
-	 *
-	 * ?????????????????????<br>
-	 * -- True: ????????????;<br>
-	 * -- False: ?????;
-	 * 
-	 */
-	public boolean					isUIGetDbData;
+    /**
+     *
+     * ?????????????????????<br>
+     * -- True: ????????????;<br>
+     * -- False: ?????;
+     *
+     */
+    public boolean					isUIGetDbData;
 
-	public BN_BaseBody httpResponse;
+    public BN_BaseBody	httpResponse;
 
-	public ET_HttpError(int taskId, int errorCode, String errorDescription) {
-		this.taskId = taskId;
-		this.errorCode = errorCode;
-		this.errorDescription = errorDescription;
-	}
+    public ET_HttpError(int taskId, int errorCode, String errorDescription) {
+        this.taskId = taskId;
+        this.errorCode = errorCode;
+        this.errorDescription = errorDescription;
+    }
 
-	public ET_HttpError(int taskId, int errorCode, String errorDescription, boolean isUIGetDbData) {
-		this.taskId = taskId;
-		this.errorCode = errorCode;
-		this.errorDescription = errorDescription;
-		this.isUIGetDbData = isUIGetDbData;
-	}
+    public ET_HttpError(int taskId, int errorCode, String errorDescription, boolean isUIGetDbData) {
+        this.taskId = taskId;
+        this.errorCode = errorCode;
+        this.errorDescription = errorDescription;
+        this.isUIGetDbData = isUIGetDbData;
+    }
 }
--- app/src/main/java/com/ppandroid/whitefm/http/bean/HM_HttpTask.java
+++ app/src/main/java/com/ppandroid/whitefm/http/bean/HM_HttpTask.java
@@ -35,4 +35,72 @@ public class HM_HttpTask {
 
     /**	DB查询数据依赖条件	*/
     public HM_DBquery dBquery;
+    /**
+     *
+     * @param context
+     * @param httpType
+     * @param url
+     * @param httpParams
+     * @param etHttpResponse
+     */
+    public HM_HttpTask(Context context, HttpType httpType, String url, HM_Base httpParams,ET_Base etHttpResponse){
+        this.context = context;
+        this.httpType = httpType;
+        this.url = url;
+        this.httpParams = httpParams;
+        this.needSaveDB = false;
+        this.needGetDBData = false;
+        this.etHttpResponse = etHttpResponse;
+        this.etDataBase = null;
+        this.etSpecialLogic = null;
+        this.dBquery = null;
+    }
+    /**
+     * @param context 不能为NULL
+     * @param httpType 请求方式，可以为NULL，默认为GET
+     * @param url 请求URL，不能为NULL
+     * @param httpParams 请求参数，可以为NULL
+     * @param needSaveDB 是否要存储DB，如果不要缓存，则为false
+     * @param needGetDBData 有网络的情况是否需要先显示缓存，如果不需要，则为false
+     * @param etHttpResponse HTTP请求结果存放对象
+     * @param etDataBase 缓存结果存放对象
+     * @param etSpecialLogic 特殊业务逻辑，目前只有TOKEN失效，如果希望TOKEN失效跳转到登陆界面，则可以传NULL
+     * @param dBquery 查询DB数据需要的条件，目前对passportID做了统一处理，如果只要根据passportID来查询，则可以传NULL
+     * @author LuoZheng
+     * @date 2015年7月13日 下午1:56:45
+     */
+    public HM_HttpTask(Context context, HttpType httpType, String url, HM_Base httpParams, boolean needSaveDB, boolean needGetDBData,
+                       ET_Base etHttpResponse, ET_DataBase etDataBase, ET_SpecialLogic etSpecialLogic, HM_DBquery dBquery) {
+        super();
+        this.context = context;
+        this.httpType = httpType;
+        this.url = url;
+        this.httpParams = httpParams;
+        this.needSaveDB = needSaveDB;
+        this.needGetDBData = needGetDBData;
+        this.etHttpResponse = etHttpResponse;
+        this.etDataBase = etDataBase;
+        this.etSpecialLogic = etSpecialLogic;
+        this.dBquery = dBquery;
+    }
+
+    /**
+     * 没有缓存数据
+     * @param context
+     * @param httpType
+     * @param url
+     * @param httpParams
+     * @param etHttpResponse
+     * @param etSpecialLogic
+     * @author LuoZheng
+     * @date 2015年7月13日 下午2:07:41
+     */
+    public HM_HttpTask(Context context, HttpType httpType, String url, HM_Base httpParams,ET_Base etHttpResponse,ET_SpecialLogic etSpecialLogic) {
+        this(context,httpType,url,httpParams,false,false,etHttpResponse,null,etSpecialLogic,null);
+    }
+
+    public HM_HttpTask(Context context, HttpType httpType, String url) {
+        this(context,httpType,url,null,false,false,null,null,null,null);
+    }
+
 }
